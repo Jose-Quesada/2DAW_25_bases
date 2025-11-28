483c976a32a84362828302f4543065b599ee2d7b
Jose-Quesada <jose.quesada@fernando3martos.com>
2025-11-28 10:14:26 +0100

    28 noviembre

--- src/app/pages/counter/counter-page.ts
+++ src/app/pages/counter/counter-page.ts
@@ -4,14 +4,17 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: 'counter-page',
-  template:`<H1>FIN DE SEMANA de {{counter}}</H1>
-  <h2>De estudiar Angular</h2>
-  <button (click)="increaseBy(1)"> Incrementar </button>`
+  templateUrl:'./counter-page.html',
+  styleUrl:'./counter-page.css'
 })
 export class CounterPage{
   counter = 10;
 
   increaseBy(value:number){
     this.counter += value;
+  }
+
+  resetCounter(){
+    this.counter = 10;
   }
 }
